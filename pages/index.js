449552bcef1f7520e5d0f371f1c133f899d10ffd
danrlleimiranda <danmiranda14@gmail.com>
2025-04-15 22:37:25 -0300

    feat: add `prettier` and scripts

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
-  return (
-    <div>Quero aprender tudo de novo</div>
-  )
+  return <div>Quero aprender tudo de novo</div>;
 }
