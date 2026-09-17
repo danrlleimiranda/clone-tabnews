c5104ccb345dfc741cc5b534969751a83d6115db
Danrllei Miranda <danrllei.miranda@g4educacao.com>
2026-09-17 12:34:49 -0300

    feat: finish status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,25 @@
 import database from "infra/database.js"
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 AS sum;");
+  const updatedAt = new Date().toISOString();
+  const databaseName = process.env.POSTGRES_DB
+  const maxConnections = await database.query("SHOW max_connections")
+  const usedConnections = await database.query({
+    text: "SELECT count(*)::int used from pg_stat_activity WHERE datname = $1;", 
+    values: [databaseName]
+  })
+  const pgVersion = await database.query("SHOW server_version")
 
-  return res.status(200).json({ sum: result?.rows })
+  return res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: pgVersion.rows[0].server_version,
+        max_connections: parseInt(maxConnections.rows[0].max_connections),
+        used_connections: usedConnections.rows[0].used
+      }
+    }
+  })
 }
 
 export default status;
