38f3bcc7ab52d24880374013d3c5066b0de67202
Danrllei Miranda <danrllei.miranda@g4educacao.com>
2026-09-17 12:33:22 -0300

    feat: add error handling

--- infra/database.js
+++ infra/database.js
@@ -1,14 +1,14 @@
 import { Client } from 'pg'
 
 async function query(queryObject) {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD
+  });
   try {
-    const client = new Client({
-      host: process.env.POSTGRES_HOST,
-      port: process.env.POSTGRES_PORT,
-      user: process.env.POSTGRES_USER,
-      database: process.env.POSTGRES_DB,
-      password: process.env.POSTGRES_PASSWORD
-    });
   
     await client.connect();
     const result = await client.query(queryObject)
@@ -16,7 +16,9 @@ async function query(queryObject) {
     return result;
 
   } catch (err) {
-    console.log(err)
+    console.error(err)
+  } finally {
+    await client.end();
   }
 }
 export default {
