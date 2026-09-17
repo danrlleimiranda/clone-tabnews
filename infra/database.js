c2b8905ee992ad25452b8f0f0bd3ed885299b117
Danrllei Miranda <danrllei.miranda@g4educacao.com>
2026-09-17 13:56:27 -0300

    feat: add sslmode

--- infra/database.js
+++ infra/database.js
@@ -6,17 +6,18 @@ async function query(queryObject) {
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === 'development' ? false : true
   });
   try {
   
     await client.connect();
     const result = await client.query(queryObject)
-    await client.end()
     return result;
 
   } catch (err) {
     console.error(err)
+    throw err
   } finally {
     await client.end();
   }
