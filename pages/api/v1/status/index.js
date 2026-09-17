b7a54eda9a06cde2c81b950763855a7a38fba09b
Danrllei Miranda <danrllei.miranda@g4educacao.com>
2026-09-16 23:43:33 -0300

    feat: add env file and database config

---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,10 @@
+import database from "../../../../infra/database.js"
+
+async function status(req, res) {
+  const result = await database.query("SELECT 1 + 1 AS sum;");
+
+
+  return res.status(200).json({ sum: result.rows })
+}
+
+export default status;
