8eae67423e1067cfaf81b3ebfb20d2f5fa350159
Danrllei Miranda <danrllei.miranda@g4educacao.com>
2026-09-18 21:10:44 -0300

    adds wait-for-postgres script

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,18 @@
+const { exec } = require("node:child_process");
+
+function checkPostgres() {
+  exec(
+    "docker exec postgres-dev pg_isready --host localhost",
+    (error, stdout, stderr) => {
+      if (stdout.search("accepting connections") === -1) {
+        process.stdout.write(".");
+        checkPostgres();
+        return;
+      }
+      console.log("\n\n O postgres está pronto para receber conexões \n");
+    },
+  );
+}
+
+process.stdout.write("Aguardando postgres ficar pronto.");
+checkPostgres();
