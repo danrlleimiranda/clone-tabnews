f5b1a06c21c441bf88736aa744bc785861b96774
Danrllei Miranda <danrllei.miranda@g4educacao.com>
2026-09-21 22:43:03 -0300

    refactor: add clearDatabase to orchestrator

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,18 +4,22 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET to api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving application status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
 
-  expect(response.status).toBe(200);
-  const responseBody = await response.json();
-  expect(responseBody.updated_at).toBeDefined();
-  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
-  expect(responseBody.dependencies.database.max_connections).toBeDefined();
-  expect(responseBody.dependencies.database.max_connections).toEqual(100);
-  expect(responseBody.dependencies.database.used_connections).toBeDefined();
-  expect(responseBody.dependencies.database.used_connections).toEqual(1);
-  expect(responseBody.dependencies.database.version).toBeDefined();
-  expect(responseBody.dependencies.database.version).toEqual("16.3");
+      expect(response.status).toBe(200);
+      const responseBody = await response.json();
+      expect(responseBody.updated_at).toBeDefined();
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+      expect(responseBody.dependencies.database.max_connections).toBeDefined();
+      expect(responseBody.dependencies.database.max_connections).toEqual(100);
+      expect(responseBody.dependencies.database.used_connections).toBeDefined();
+      expect(responseBody.dependencies.database.used_connections).toEqual(1);
+      expect(responseBody.dependencies.database.version).toBeDefined();
+      expect(responseBody.dependencies.database.version).toEqual("16.3");
+    });
+  });
 });
